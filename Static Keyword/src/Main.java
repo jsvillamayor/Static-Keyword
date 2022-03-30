9194166a0be66355fa580b1027f4c8ef198323e7
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-30 10:26:58 -0700

    Add files via upload

---
+++ Static Keyword/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Friend friend1 = new Friend("Spongebob");
+		Friend friend2 = new Friend("Patrick");
+		Friend friend3 = new Friend("Squidward");
+		
+		System.out.println(Friend.numberOfFriends);
+
+	}
+
+}
